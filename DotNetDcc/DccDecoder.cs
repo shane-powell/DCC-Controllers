647161b565c2943e8b0f542199e8f50b69a25c4e
shane <shane.powell@tftp.biz>
2019-07-24 12:02:37 +0100

    Initial commit.

---
+++ DotNetDcc/DccDecoder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DccControllersLibNetStandard
+{
+    public class DccDecoder
+    {
+        private int address = 0;
+
+        private string name = string.Empty;
+
+        /// <summary>
+        /// The last speed value given send to the decoder
+        /// 0 - 126 (-1 for emergency stop)
+        /// </summary>
+        private int speed = 0;
+
+        /// <summary>
+        /// 1 = Forward, 0 = Reverse
+        /// </summary>
+        private int direction = 1;
+
+        public int Address
+        {
+            get
+            {
+                return this.address;
+            }
+            set
+            {
+                this.address = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+    }
+}
